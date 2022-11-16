241f7427a45cb122144b6a0cac72ca04b766cd08
GrandhiBharath <96789976+GrandhiBharath@users.noreply.github.com>
2022-11-16 09:00:04 +0530

    Update 1st -ve in list.py

--- day8/1st -ve in list.py
+++ day8/1st -ve in list.py
@@ -1,20 +1,25 @@
 l=list(map(int,input().split()))
 k=int(input())
 i,j=0,0
+t=[]
 while(j<len(l)):
-    if(l[j]<0):
-        print(l[j],end=" ")
-        if(j==(len(l)-1)):
-            break
+    if((j-i+1)<k):
+        j+=1
+    if(l[j]<0): 
+        t.append(l[j])
+    if((j-i+1)==k):
+        if(len(t)==0):
+            print("0",end=" ")
+        else:
+            if(t[0]==l[i]):
+                print(t.pop(0),end=" ")
+            else:
+                print(t[0],end=" ")
         i+=1
-        j=i
-    elif((j-i+1)==k):
-        print("0",end=" ")
-        if(j==(len(l)-1)):
-            break
-        i=i+1
-        j=i
-    else:
-        j=j+1
+        j+=1
         
+    
+        
+        
+
     
