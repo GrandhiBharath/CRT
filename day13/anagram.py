e268e762c5f0853e97c2557ce06020823ad82da8
GrandhiBharath <96789976+GrandhiBharath@users.noreply.github.com>
2022-11-16 09:24:49 +0530

    Update anagram.py

--- day13/anagram.py
+++ day13/anagram.py
@@ -25,7 +25,25 @@ if(len(s1)==len(s2)):
 else:
     print("not anagram")
     
-        
+
+'''
+f=[0]*26
+s1=input().lower()
+s2=input().lower()
+for i in s1:
+    f[ord(i)-97]+=1
+for i in s2:
+    f[ord(i)-97]-=1
+w=0
+for i in f:
+    if(i!=0):
+        w=1
+        break
+if(w==1):
+    print("not anagram")
+else:
+    print("anagram")
+'''
 
             
     
