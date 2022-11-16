be80ee8f7feb79cf2b26956a608fe52def14a236
GrandhiBharath <96789976+GrandhiBharath@users.noreply.github.com>
2022-11-16 20:47:30 +0530

    Update prime factorization.py

--- day14/prime factorization.py
+++ day14/prime factorization.py
@@ -1,20 +1,25 @@
+import math
 n=int(input())
-m=n
 def prime(n):
     c=0
-    for i in range(2,n//2+1):
+    for i in range(2,int(math.sqrt(n))+1):
         if(n%i==0):
             c=1
             break
     if(c==0):
-        return true
-def primefactors(n):
-    for i in range(2,n):
-        if(n%i==0):
-            print(i,end=" ")
-            n=n//i
-            
-if(prime(n)):
-    print(n)
+        return 1
+    else:
+        return 0
+while(n!=1):
+    if(prime(n)):
+        print(n)
+        break
+    else:
+        for i in range(2,int(math.sqrt(n))+1):
+            if(n%i==0):
+                print(i,end=" ")
+                n=n//i
+                break
+
     
         
