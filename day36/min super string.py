0b5ec89ed77c071a86582475cbbf5d84c4474459
GrandhiBharath <96789976+GrandhiBharath@users.noreply.github.com>
2022-12-22 21:48:43 +0530

    Add files via upload

---
+++ day36/min super string.py
@@ -0,0 +1,27 @@
+s1=input()
+s2=input()
+s1="0"+s1
+s2="0"+s2
+m=[]
+for i in range(len(s2)):
+    t=[0]*(len(s1))
+    m.append(t)
+for i in range(1,len(s2)):
+    for j in range(1,len(s1)):
+        if(s2[i]==s1[j]):
+            a=m[i-1][j-1]+1
+            m[i][j]=a
+        else:
+            m[i][j]=max(m[i][j-1],m[i-1][j])
+for i in range(len(s2)):
+    for j in range(len(s1)):
+        print(m[i][j],end=" ")
+    print()
+ma=0
+for i in range(len(s2)):
+    for j in range(len(s1)):
+        if(m[i][j]>ma):
+            ma=m[i][j]
+a=len(s1)-ma-1
+b=len(s2)-ma-1
+print(a+b+ma)
