495033768b9e14d30718bdf9097ceb50a497aaa8
GrandhiBharath <96789976+GrandhiBharath@users.noreply.github.com>
2023-02-21 13:23:44 +0530

    Delete day13 directory

--- day13/count of each charac.py
+++
@@ -1,9 +0,0 @@
-s=input()
-d={}
-for i in s:
-    if i not in d.keys():
-            d[i]=1
-    else:
-            d[i]+=1
-for i in d.keys():
-    print(i,'->',d[i])
--- day13/count of symbols.py
+++
@@ -1,8 +0,0 @@
-s=input()
-c=0
-for i in s:
-    if((ord(i)>=65 and ord(i)<=90) or (ord(i)>=97 and ord(i)<=122) or (ord(i)>=48 and ord(i)<=57)):
-        continue
-    else:
-        c=c+1
-print(c)
